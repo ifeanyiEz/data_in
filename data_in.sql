2b88c8cb151cf7b56c694f3d0dd951ed88979f36
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2022-11-07 02:26:34 +0100

    Individuals contacted via organic and adwords channels

--- data_in.sql
+++ data_in.sql
@@ -4,4 +4,13 @@
 
 SELECT name, primary_poc, sales_rep_id
 FROM accounts
-WHERE name IN ('Walmart', 'Target', 'Nordstrom')
+WHERE name IN ('Walmart', 'Target', 'Nordstrom');
+
+
+/*Use the web_events table to find all information regarding individuals who were contacted via the channel of organic or adwords.*/
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords')
+ORDER BY channel
+LIMIT 20;
